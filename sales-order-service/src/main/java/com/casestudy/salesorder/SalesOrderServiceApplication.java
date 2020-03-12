c9dd61ade0d090b410d2dfa00ba7b0ad0964f3d0
sandisan <sandicha@in.ibm.com>
2020-03-12 21:08:47 +0530

    Update SalesOrderServiceApplication.java

--- sales-order-service/src/main/java/com/casestudy/salesorder/SalesOrderServiceApplication.java
+++ sales-order-service/src/main/java/com/casestudy/salesorder/SalesOrderServiceApplication.java
@@ -3,11 +3,9 @@ package com.casestudy.salesorder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 //@EnableDiscoveryClient
-@EnableFeignClients("com.casestudy.salesorder")
+//@EnableFeignClients("com.casestudy.salesorder")
 @SpringBootApplication
 @EnableCircuitBreaker
 public class SalesOrderServiceApplication {
